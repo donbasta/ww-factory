43d18cff47ee3758158c78a58a6187d5d823e42e
donbasta <faddilafarras@gmail.com>
2020-11-28 18:44:01 +0700

    show supplies from supplier

--- src/components/BahanSupplier.js
+++ src/components/BahanSupplier.js
@@ -2,7 +2,14 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class BahanSupplier extends React.Component {
-    lihatBahanSupplier() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bahanInSupplier: []
+        };
+    }
+
+    componentDidMount() {
         const instance = axios.create({
             baseURL: "http://localhost:3030/"
         });
@@ -10,14 +17,43 @@ class BahanSupplier extends React.Component {
             .then(res => {
                 const data = res.data;
                 console.log(data);
+                this.setState({ bahanInSupplier: data });
             })
             .catch ((error) => {
                 console.log("error fetching from api", error);
+                return []
             })
     }
+
+    renderTableData() {
+        return this.state.bahanInSupplier.map((bahan, index) => {
+            const { id_bahan, nama_bahan, harga_satuan } = bahan;
+            return (
+                <tr key={ id_bahan }>
+                    <td>{ id_bahan }</td>
+                    <td>{ nama_bahan }</td>
+                    <td>{ harga_satuan }</td>
+                </tr>
+            )
+        })
+    }
+
     render() {
-        this.lihatBahanSupplier();
-        return <h1>test, lihat 'console' </h1>;
+        return (
+            <div>
+                <h1> Daftar Bahan di Supplier </h1>
+                <table id='bahanSupplier'>
+                    <tbody>
+                        <tr>
+                            <td>ID Bahan</td>
+                            <td>Nama Bahan</td>
+                            <td>Harga Satuan</td>
+                        </tr>
+                        { this.renderTableData() }
+                    </tbody>
+                </table>
+            </div>
+        )
     }
 }
 
